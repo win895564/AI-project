6268787ef182071d731d8de036a8d6da068a376e
win895564 <90327297+win895564@users.noreply.github.com>
2022-03-06 08:36:15 +0800

    Update store name (selenium).py

--- store name (selenium).py
+++ store name (selenium).py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from bs4 import BeautifulSoup 
 import requests
 
-
-driver=webdriver.Chrome()
+chrome_options=Options()
+chrome_options.add_argument('--headless')
+driver=webdriver.Chrome(chrome_options=chrome_options)
 driver.get('https://www.google.com.tw/maps/search/%E5%9E%82%E6%A5%8A%E9%87%8C+%E5%85%A8%E5%AE%B6/@23.4734576,120.4184991,14z/data=!3m1!4b1?hl=zh-TW')
 soup = BeautifulSoup(driver.page_source,"lxml")
 all_reviews = soup.find_all(class_='MVVflb-haAclf V0h1Ob-haAclf-d6wfac MVVflb-haAclf-uxVfW-hSRGPd')
