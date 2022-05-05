42076cfe7e1eef03cc0d19893d94241fc02f976f
阿丹 <90327297+win895564@users.noreply.github.com>
2022-05-05 20:06:38 +0800

    爬取使用者IP

---
+++ CatchIP.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 01:00:05 2022
+
+@author: user
+"""
+from bs4 import BeautifulSoup as bs
+import requests 
+def catch_ip():
+    r=requests.get('https://myip.com.tw/')
+    soup=bs(r.text,'html.parser')
+    soup_ip=soup.find('font')
+    return soup_ip.text
